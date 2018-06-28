8f63b9317c63d1fc2a5487dc361082fe6d5ae53d
Abhinav Nigam <abnig19@gmail.com>
2018-06-27 21:55:33 -0400

    refactored code

--- find-employee-rest/src/main/java/rest/mainrunner/RestApplication.java
+++ find-employee-rest/src/main/java/rest/mainrunner/RestApplication.java
@@ -1,11 +1,11 @@
-package mainrunner;
+package rest.mainrunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @EnableCircuitBreaker
-@SpringBootApplication(scanBasePackages = {"rest", "model", "config"})
+@SpringBootApplication(scanBasePackages = {"rest", "model"})
 public class RestApplication {
 
     public static void main(String[] args) {
