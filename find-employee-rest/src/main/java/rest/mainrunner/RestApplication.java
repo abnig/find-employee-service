4ef46d1082607e075c405120a42a0a71e364b0c8
Abhinav Nigam <abnig19@gmail.com>
2018-10-22 23:12:02 -0400

    added employee service as eureka client

--- find-employee-rest/src/main/java/rest/mainrunner/RestApplication.java
+++ find-employee-rest/src/main/java/rest/mainrunner/RestApplication.java
@@ -3,7 +3,9 @@ package rest.mainrunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication(scanBasePackages = {"rest", "model"})
 public class RestApplication {
