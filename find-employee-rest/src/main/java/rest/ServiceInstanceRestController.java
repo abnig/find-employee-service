e160b104a0e4890dc3448bc2c88d0a6ee7e24269
Abhinav Nigam <abnig19@gmail.com>
2019-03-26 23:33:39 -0400

    [abnig] - updated to Finchley release

--- find-employee-rest/src/main/java/rest/ServiceInstanceRestController.java
+++ find-employee-rest/src/main/java/rest/ServiceInstanceRestController.java
@@ -1,5 +1,6 @@
 package rest;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
 @RestController
 @RequestMapping(value = "/service-instances", produces = "application/json")
 public class ServiceInstanceRestController {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	private EurekaClient eurekaClient;
 
 	@RequestMapping("/{applicationName}")
 	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
-		return this.discoveryClient.getInstances(applicationName);
+		List<ServiceInstance> serviceInstances = this.discoveryClient.getInstances(applicationName); 
+		Collections.rotate(serviceInstances, 1);//serviceInstances.stream().filter(o -> o.)
+		return serviceInstances;
+		
+	}
+	
+	@RequestMapping("eureka/{applicationName}")
+	public InstanceInfo instanceInfoByApplicationName(@PathVariable String applicationName) {
+		InstanceInfo instanceInfo = this.eurekaClient.getNextServerFromEureka(applicationName, false);
+		return instanceInfo;
 	}
 	
 	@RequestMapping("/services")
